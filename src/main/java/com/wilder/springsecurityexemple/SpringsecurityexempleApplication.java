d45a7ddaf1ee6bd614884db8248481efc2c472f7
haroune_b <125916355+harouneboutlookfr@users.noreply.github.com>
2024-01-18 17:14:09 +0000

    first commit

---
+++ src/main/java/com/wilder/springsecurityexemple/SpringsecurityexempleApplication.java
@@ -0,0 +1,13 @@
+package com.wilder.springsecurityexemple;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringsecurityexempleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringsecurityexempleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wilder/springsecurityexemple/SpringsecurityexempleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wilder.springsecurityexemple;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringsecurityexempleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
