0966db7577cdfcb687eb5615b97d95fa6812093a
haroune_b <125916355+harouneboutlookfr@users.noreply.github.com>
2024-01-18 18:49:48 +0000

    UserDetails

--- src/main/java/com/wilder/springsecurityexemple/configuration/MySecurityConfig.java
+++ src/main/java/com/wilder/springsecurityexemple/configuration/MySecurityConfig.java
@@ -2,26 +2,58 @@ package com.wilder.springsecurityexemple.configuration;
 
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import java.util.List;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @EnableWebSecurity
 public class MySecurityConfig {
-@Bean
-public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-	http.authorizeHttpRequests((authz) -> {
-		try {
-			authz
-				.anyRequest().authenticated()
-				.and().formLogin()
-				.and().httpBasic();
-		} catch (Exception e) {
 
-			e.printStackTrace();
-		}
-	});
-	return http.build();
-}
+	@Bean
+	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+		http.authorizeHttpRequests((authz) -> {
+			try {
+				authz
+				
+						.anyRequest().authenticated()
+						.and().formLogin()
+						.and().httpBasic();
+			} catch (Exception e) {
+
+				e.printStackTrace();
+			}
+		});
+		return http.build();
+	}
+
+	// User bean
+	@Bean
+	public InMemoryUserDetailsManager userDetailsService() {
+
+		PasswordEncoder encoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
+
+		UserDetails user = User
+				.withUsername("user")
+				.password(encoder.encode("password"))
+				.roles("USER")
+				.build();
+
+		// Admin
+		UserDetails admin = User
+				.withUsername("admin")
+				.password(encoder.encode("12345678"))
+				.roles("ADMIN")
+				.build();
+
+		return new InMemoryUserDetailsManager(List.of(user, admin));
+	}
 }
--- src/main/java/com/wilder/springsecurityexemple/controller/HelloWorldController.java
+++ src/main/java/com/wilder/springsecurityexemple/controller/HelloWorldController.java
@@ -5,11 +5,15 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
- 
-@CrossOrigin (origins ="https://automatic-space-guide-j66qw6qw5x9fp5xp-8080.app.github.dev")
-public class HelloWorldController {
+
+ public class HelloWorldController {
     @GetMapping("/")
     public String hello() {
         return "Hello World!!!";
+    }
+
+    @GetMapping("/admin")
+    public String admin() {
+        return "Hello Admin!!!";
     }
 }
